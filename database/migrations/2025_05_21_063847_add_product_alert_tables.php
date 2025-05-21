54b6421ffe6cbc81120a86ff16bcebb4f367ac28
Bennok <bennnok@gmail.com>
2025-05-21 08:44:33 +0200

    add migrations

---
+++ database/migrations/2025_05_21_063847_add_product_alert_tables.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_alerts', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->foreignUuid('product_id')->constrained('products', 'uuid');
+            $table->integer('alert_at_quantity')->default(0);
+            $table->timestamps();
+        });
+
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->foreignUuid('product_id')->constrained('products', 'uuid');
+            $table->foreignUuid('alert_id');
+            $table->string('type');
+            $table->string('message');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_alerts');
+        Schema::dropIfExists('notifications');
+    }
+};
---
+++ database/migrations/2025_05_21_063927_add_storage_movement_tables.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->foreignUuid('storage_id')->constrained('storages', 'uuid');
+            $table->foreignUuid('product_id')->constrained('products', 'uuid');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+
+        Schema::create('storage_movements', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->foreignUuid('storage_id')->constrained('storages', 'uuid');
+            $table->foreignUuid('product_id')->constrained('products', 'uuid');
+            $table->enum('movement', ['in', 'out']);
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stocks');
+        Schema::dropIfExists('storage_movements');
+    }
+};
